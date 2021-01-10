f7355ab02e7b46a3cbcb6f2165585b1e202b3b90
Tanya Srivastava <71485040+thetanyasrivastava@users.noreply.github.com>
2021-01-10 21:49:37 +0530

    LC- 142 Linked List II Created
    Return the node where cycle begins.

---
+++ 142_LinkedListII.cpp
@@ -0,0 +1,35 @@
+/**
+ * Definition for singly-linked list.
+ * struct ListNode {
+ *     int val;
+ *     ListNode *next;
+ *     ListNode(int x) : val(x), next(NULL) {}
+ * };
+ */
+class Solution {
+public:
+    ListNode *detectCycle(ListNode *head) {
+        if(head==NULL || head->next==NULL)
+            return NULL;
+        
+        ListNode *fast=head, *slow=head;
+        while(fast!=NULL && fast->next!=NULL)
+        {
+            fast=fast->next->next;
+            slow=slow->next;
+            if(slow==fast)
+            {
+                slow=head;
+                while(slow!=fast)
+                {
+                    slow=slow->next;
+                    fast=fast->next;
+                }
+                return slow;
+            }       
+        }
+        return NULL;
+         
+        
+    }
+};
