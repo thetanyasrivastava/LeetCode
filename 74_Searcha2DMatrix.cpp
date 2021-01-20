72bf01641e069d9c617d9ccec0ee91bd6b721729
Tanya Srivastava <71485040+thetanyasrivastava@users.noreply.github.com>
2021-01-20 16:15:47 +0530

    LC - 74. Search a 2D Matrix Created

---
+++ 74_Searcha2DMatrix.cpp
@@ -0,0 +1,22 @@
+class Solution {
+public:
+    bool searchMatrix(vector<vector<int>>& matrix, int target) {
+     int n = matrix.size();
+    int m = matrix[0].size();
+        
+        int i=0;
+        int j  = m-1;
+        
+        while(i>=0 && i<n && j>=0 && j<m)
+        {
+            cout<<matrix[i][j];
+            if(target==matrix[i][j])
+                return true;
+            else if(target<matrix[i][j])
+                j--;
+            else 
+                i++;
+        }
+        return false;
+    }
+};
