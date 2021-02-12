57e27a05c0b1ee9edf02ae8f38f49b6ecfc2ba4f
Tanya Srivastava <71485040+thetanyasrivastava@users.noreply.github.com>
2021-02-12 16:51:39 +0530

    LC-1342 Number of Steps to Reduce a no. to Zero

---
+++ 1342_NumberofStepstoReduceaNumbertoZero.cpp
@@ -0,0 +1,19 @@
+class Solution {
+public:
+    int numberOfSteps (int num) {
+        int count=0;
+        if(num==0)
+            return 0;
+        while(num>0)
+        {
+            if((num&1)==0)
+                num = num >> 1;
+            else
+                num =num - 1;
+            
+            count++;
+           // cout<<count<<" "<<num<<endl;
+        }
+        return count;
+    }
+};
