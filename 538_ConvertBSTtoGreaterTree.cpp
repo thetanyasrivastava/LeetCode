3fd24c81037f575f9de8f85a86f7706ff9be6850
Tanya Srivastava <71485040+thetanyasrivastava@users.noreply.github.com>
2021-03-19 00:52:04 +0530

    LC - 538. Convert BST to Greater Tree

---
+++ 538_ConvertBSTtoGreaterTree.cpp
@@ -0,0 +1,31 @@
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+ * };
+ */
+class Solution {
+public:
+    TreeNode* convertBST(TreeNode* root) {
+        if(!root)
+            return root;
+        int sum=0;
+        greater(root,sum);
+        return root;
+    }
+    void greater(TreeNode* root,int &sum)
+    {
+        if(!root) return;
+        greater(root->right,sum);
+        
+        sum+=root->val;
+        root->val=sum;
+        
+        greater(root->left,sum);
+    }
+};
