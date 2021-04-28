0be55f83c0437daa6a692cb4433f12d80cee49e5
Tanya Srivastava <71485040+thetanyasrivastava@users.noreply.github.com>
2021-04-28 15:06:59 +0530

    LC - 27. Remove Element

---
+++ 27_RemoveElement.cpp
@@ -0,0 +1,26 @@
+class Solution {
+public:
+    int removeElement(vector<int>& nums, int val) {
+        int n=nums.size();
+        //int j=0;
+        
+        /*for(int i=0;i<n;i++)
+        {
+            if(nums[i]!=val)
+                nums[j++]=nums[i];
+        }
+        
+        return j;*/
+        
+        for(int i=0;i<n;i++)
+        {
+            if(nums[i]==val)
+            {
+                nums[i]=nums[n-1];
+                --n;
+                --i;
+            }
+        }
+        return n;
+    }
+};
