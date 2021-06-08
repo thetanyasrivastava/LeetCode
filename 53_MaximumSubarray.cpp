71988c803bcf6d4773764cedc76c832ff07f5752
Tanya Srivastava <71485040+thetanyasrivastava@users.noreply.github.com>
2021-06-08 14:42:08 +0530

    LC - 53. Maximum Subarray

---
+++ 53_MaximumSubarray.cpp
@@ -0,0 +1,30 @@
+class Solution {
+public:
+    int maxSubArray(vector<int>& nums) { 
+        /*
+        //Kadane's Algorithm
+        int sum=0;
+        int maxi=nums[0];
+        for(int i=0;i<nums.size();i++)
+        {
+            sum+=nums[i];                   
+            if(sum>maxi)
+                maxi=sum;
+            if(sum<0)
+                sum=0;
+        }
+        return maxi;*/
+        
+        
+        int n=nums.size();
+        int sum=0, maxi=nums[0];
+        for(int i=0;i<n;i++)
+        {
+            sum+=nums[i];
+            maxi=max(sum,maxi);
+            if(sum<0)
+                sum=0;
+        }
+        return maxi;
+    }
+};
