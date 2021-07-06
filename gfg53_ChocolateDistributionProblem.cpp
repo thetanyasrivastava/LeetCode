abad6c8fbf4841c607c4ef5e14754a7983ce3252
Tanya Srivastava <71485040+thetanyasrivastava@users.noreply.github.com>
2021-07-06 19:40:32 +0530

    GFG - 53. Chocolate Distribution Problem

---
+++ gfg53_ChocolateDistributionProblem.cpp
@@ -0,0 +1,23 @@
+//
+
+class Solution{
+    public:
+    long long findMinDiff(vector<long long> a, long long n, long long m){
+    //code
+    sort(a.begin(),a.end());
+    long long i=0;
+    long long minm=LLONG_MAX;
+    long long maxm=LLONG_MIN;
+    long long ans=LLONG_MAX;
+    
+    //while((i+m-1)<n)
+    while(i<=n-m)
+    {
+        minm=a[i];
+        maxm=a[i+m-1];
+        ans=min(ans,maxm-minm);
+        i++;
+    }
+    return ans;
+    }   
+};
