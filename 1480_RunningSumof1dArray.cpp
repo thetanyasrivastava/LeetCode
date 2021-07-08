bfc8d50c219cf85e707f92437889e34d6a3a6377
Tanya Srivastava <71485040+thetanyasrivastava@users.noreply.github.com>
2021-07-08 09:23:56 +0530

    Updated LC - 1480. Running Sum of 1-D Array

--- 1480_RunningSumof1dArray.cpp
+++ 1480_RunningSumof1dArray.cpp
@@ -1,5 +1,7 @@
 class Solution {
 public:
+   /*//Method -1 
+    //T.C. = O(N)   S.C. = O(N)
     vector<int> runningSum(vector<int>& nums) {
         vector<int> res;
         int sum=0;
@@ -9,5 +11,15 @@ public:
             res.push_back(sum);
         }
         return res;
+    }*/
+    
+    //Method - 2
+    //T.C. = O(N)   S.C. = O(1)
+    vector<int> runningSum(vector<int>& nums) {
+        for(int i=1;i<nums.size();i++)
+        {
+            nums[i]+=nums[i-1];
+        }
+        return nums;
     }
 };
